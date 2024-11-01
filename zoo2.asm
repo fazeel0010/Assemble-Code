436d886e3bd68fce20f6ca83daa961bd1190e268
Ainos Dive <fazeel0010@gmail.com>
2024-11-01 23:05:49 +0500

    update

--- zoo2.asm
+++ zoo2.asm
@@ -1,76 +1,139 @@
 section .data
-    max_names equ 100
-    max_length equ 100
-    prompt db "Enter name: ", 0
-    names times max_names * max_length db 0
+    main_menu db "Main Menu:", 0
+    prompt_title db "1. Enter title", 0
+    prompt_author db "2. Enter author", 0
+    prompt_exit db "3. Exit", 0
+    enter_title db "Enter title: ", 0
+    enter_author db "Enter author: ", 0
+    display_title db "Title: ", 0
+    display_author db "Author: ", 0
 
 section .bss
-    i resq 1
-    buf resb max_length
+    title resb 100
+    author resb 100
+    choice resb 2
 
 section .text
     global _start
 
 _start:
-    ; Initialize loop counter
-    mov qword [i], 0
+    ; Main loop
+main_loop:
+    ; Print main menu
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, main_menu
+    mov rdx, 11
+    syscall
 
-loop_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt_title
+    mov rdx, 15
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt_author
+    mov rdx, 16
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt_exit
+    mov rdx, 11
+    syscall
+
+    ; Read choice
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, choice
+    mov rdx, 2
+    syscall
+
+    ; Handle choice
+    mov rsi, choice
+    movsil
+    cmp rsi, '1'
+    je enter_title
+
+    cmp rsi, '2'
+    je enter_author
+
+    cmp rsi, '3'
+    je exit_program
+
+    jmp main_loop
+
+enter_title:
     ; Print prompt
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; file descriptor (stdout)
-    mov rsi, prompt
-    mov rdx, 12         ; length of prompt
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, enter_title
+    mov rdx, 13
     syscall
 
-    ; Read name
-    mov rax, 0          ; sys_read
-    mov rdi, 0          ; file descriptor (stdin)
-    mov rsi, buf        ; buffer to store input
-    mov rdx, max_length ; max length to read
+    ; Read title
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, title
+    mov rdx, 100
     syscall
 
-    ; Check if read operation was successful
-    cmp rax, 0
-    jl error
+    ; Null-terminate title
+    mov byte [title + rax], 0
 
-    ; Null-terminate the input string
-    mov byte [buf + rax], 0
+    ; Display title
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, display_title
+    mov rdx, 8
+    syscall
 
-    ; Store input in names array
-    mov rsi, [i]
-    imul rsi, max_length
-    lea rdi, [names + rsi]
-    mov rsi, buf
-    mov rdx, max_length
-    call strcpy
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, title
+    mov rdx, 100
+    syscall
 
-    ; Increment loop counter
-    add qword [i], 1
+    jmp main_loop
 
-    ; Check loop condition
-    cmp qword [i], max_names
-    jl loop_start
+enter_author:
+    ; Print prompt
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, enter_author
+    mov rdx, 14
+    syscall
 
+    ; Read author
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, author
+    mov rdx, 100
+    syscall
+
+    ; Null-terminate author
+    mov byte [author + rax], 0
+
+    ; Display author
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, display_author
+    mov rdx, 9
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, author
+    mov rdx, 100
+    syscall
+
+    jmp main_loop
+
+exit_program:
     ; Exit program
     xor rax, rax
     xor rdi, rdi
     mov rax, 60
     syscall
-
-error:
-    ; Handle error
-    mov rax, 60
-    mov rdi, 1
-    syscall
-
-; Simple strcpy function
-strcpy:
-    mov rsi, [rsi]
-    mov [rdi], rsi
-    add rdi, 1
-    add rsi, 1
-    dec rdx
-    cmp rdx, 0
-    jg strcpy
-    ret
